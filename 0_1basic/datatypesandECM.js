4bd8413b7fe5483ea1979dc8b95adb15d6c62ff6
Soumapriyo Mondal <103531896+SoumapriyoM@users.noreply.github.com>
2024-03-01 20:38:44 +0000

    02-03-24

---
+++ 0_1basic/datatypesandECM.js
@@ -0,0 +1,28 @@
+"use strict"//treat all js as newer version(its automated nowadays and make use latest js)
+//alert("hello") // we are using node js but not web
+
+// console.log(3
+//     +3) 
+// console.log("popy") code readibilty must maintain for good practice
+
+let name ="hitest"
+let age = 18
+let islog =false
+
+// number=> 2 to power 53 upto
+// bigint
+// string =>""
+//boolean => true/false
+// null =>stand alone value to show empty.you can assign null to a variables.
+// undefined => value hasnt asigned.undef and null are huge diff
+// symbol (used in react)
+
+
+//object
+
+console.log(typeof "appu");//string
+console.log(typeof null);//object
+
+console.log(typeof undefined);//undefined
+
+
